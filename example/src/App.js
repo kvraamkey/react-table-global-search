31553e710a0814f1adc02f099b4e7158cbf2292e
ramki <ramki.ayyamperumal@everestdx.com>
2020-07-06 00:33:56 +0530

    [FEAT] table search hooks

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,81 @@
-import React from 'react'
+import React from 'react';
 
-import { ExampleComponent } from 'table-global-search'
-import 'table-global-search/dist/index.css'
+import { useTableSearch } from 'table-global-search';
+import 'table-global-search/dist/index.css';
+
+const getUser = async () => {
+  const resp = await fetch('https://jsonplaceholder.typicode.com/users/');
+  const data = await resp.json();
+  return { data };
+};
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  const [searchVal, setSearchVal] = React.useState(null);
 
-export default App
+  const { filteredData, loading } = useTableSearch({
+    searchVal,
+    retrieve: getUser
+  });
+
+  return (
+    <div className='w3-container'>
+      <h3>Make a Search Engine for Any Table in React</h3>
+      <p>
+        This method is a generic one and would work with any type of table that
+        involves feeding a data source to it in order to render the rows.
+      </p>
+      <div className='search-bar w3-margin-bottom w3-margin-top'>
+        <input
+          onChange={(e) => setSearchVal(e.target.value)}
+          type='search'
+          className='w3-input w3-light-black w3-border w3-text-gray'
+          placeholder='Filter by tags and attributes or search by keyword'
+        />
+      </div>
+
+      <table className='w3-table-all w3-card'>
+        <thead>
+          <tr className='w3-light-grey'>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading && (
+            <tr>
+              <td>loading</td>
+            </tr>
+          )}
+          {!loading &&
+            filteredData.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                  <address>
+                    {Object.values(user.address)
+                      .filter((val) => typeof val !== 'object')
+                      .join(', ')}
+                  </address>
+                </td>
+                <td>{user.phone}</td>
+                <td>{user.website}</td>
+              </tr>
+            ))}
+          {!loading && filteredData.length === 0 && (
+            <tr>
+              <td>No data</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,55 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from 'react';
+import styles from './styles.module.css';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export const useTableSearch = ({ searchVal, retrieve }) => {
+  const [filteredData, setFilteredData] = React.useState([]);
+  const [origData, setOrigData] = React.useState([]);
+  const [searchIndex, setSearchIndex] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  const crawl = (user, allValues) => {
+    if (!allValues) allValues = [];
+    for (var key in user) {
+      if (typeof user[key] === 'object') crawl(user[key], allValues);
+      else allValues.push(user[key] + ' ');
+    }
+    return allValues;
+  };
+
+  const fetchData = async () => {
+    const { data: users } = await retrieve();
+    setOrigData(users);
+    setFilteredData(users);
+
+    const searchInd = users.map((user) => {
+      const allValues = crawl(user);
+      return { allValues: allValues.toString() };
+    });
+    setSearchIndex(searchInd);
+
+    if (users) setLoading(false);
+  };
+
+  React.useEffect(() => {
+    setLoading(true);
+    fetchData();
+  }, [retrieve]);
+
+  React.useEffect(() => {
+    if (searchVal) {
+      const reqData = searchIndex.map((user, index) => {
+        if (user.allValues.toLowerCase().indexOf(searchVal.toLowerCase()) >= 0)
+          return origData[index];
+        return null;
+      });
+      setFilteredData(
+        reqData.filter((user) => {
+          if (user) return true;
+          return false;
+        })
+      );
+    } else setFilteredData(origData);
+  }, [searchVal, origData, searchIndex]);
+
+  return { filteredData, loading };
+};
