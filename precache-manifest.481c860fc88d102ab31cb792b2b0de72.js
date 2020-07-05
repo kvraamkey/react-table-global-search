3b35371ce2da57f8eaa8e9a0b837267c78b6dee4
ramki <ramki.ayyamperumal@everestdx.com>
2020-07-06 01:07:43 +0530

    Updates

---
+++ precache-manifest.481c860fc88d102ab31cb792b2b0de72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac8ce7d35667e3ab81f8d646277281b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f852d198cf9d4e2676f",
+    "url": "./static/css/main.d8d2fb05.chunk.css"
+  },
+  {
+    "revision": "bd088afaa6b6669e3b0d",
+    "url": "./static/js/2.952bcbce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.952bcbce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f852d198cf9d4e2676f",
+    "url": "./static/js/main.7bb33dde.chunk.js"
+  },
+  {
+    "revision": "c2e24137b4a45402da05",
+    "url": "./static/js/runtime-main.a6b73c87.js"
+  }
+]);
