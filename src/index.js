2b4a314bf77e96ba2816af2a981c7cb1db758cfc
ramki <ramki.ayyamperumal@everestdx.com>
2020-07-06 01:49:07 +0530

    [QUAL] removed unused lines

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './styles.module.css';
+// import styles from './styles.module.css';
 
 export const useTableSearch = ({ searchVal, retrieve }) => {
   const [filteredData, setFilteredData] = React.useState([]);
